3cc10842c23ecc35a48f266427953b7fc2e7fb6c
Vrund Mirani <vrundmmirani@gmail.com>
2026-06-24 19:51:04 +0530

    training added

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 from cnnClassifier import logger
+
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
-try: 
+
+try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+
 except Exception as e:
     logger.exception(e)
     raise e
 
 
-
-
 STAGE_NAME = "Prepare base model"
+
 try:
-    logger.info(f"******************")
+    logger.info("******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info("******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
+    model_training = ModelTrainingPipeline()
+    model_training.main()
+
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+
 except Exception as e:
     logger.exception(e)
     raise e
